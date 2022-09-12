4b64068849e1b6d71801709b3f4f56925c303f5f
alexandre <alexandre.alcuvilla@bjtmail.com>
2022-09-13 00:45:40 +0200

    Extract `DestinationsViewModel` to it's own file

--- DestinationGuide/View Controllers/DestinationsViewController.swift
+++ DestinationGuide/View Controllers/DestinationsViewController.swift
@@ -8,59 +8,6 @@
 import UIKit
 import Combine
 
-class DestinationsViewModel {
-    @Published var array = [Destination]()
-    @Published var selectedDestination: DestinationDetails?
-    @Published var error: String?
-    
-    private var service: DestinationFetchingService
-    
-    init(service: DestinationFetchingService) {
-        self.service = service
-        getDestinations()
-    }
-    
-    func getDestinations() {
-        service.getDestinations { destinations in
-            self.array = Array(try! destinations.get()).sorted(by: { $0.name < $1.name })
-        }
-    }
-    
-    func getDestinationDetails(with id: Destination.ID) {
-        service.getDestinationDetails(for: id) { result in
-            DispatchQueue.main.async {
-                switch result {
-                case let .success(details):
-                    self.selectedDestination = details
-                case let .failure(error):
-                    self.error = error.localizedDescription
-                }
-            }
-            
-        }
-    }
-    
-    func numberOfItems() -> Int {
-        array.count
-    }
-    
-    func destination(for index: Int) -> Destination {
-        array[index]
-    }
-    
-    func title() -> String {
-        "Toutes nos destinations"
-    }
-    
-    func alertTitle() -> String{
-        "Erreur"
-    }
-    
-    func alertActionTitle() -> String {
-        "Annuler"
-    }
-}
-
 class DestinationsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     var viewModel: DestinationsViewModel!
