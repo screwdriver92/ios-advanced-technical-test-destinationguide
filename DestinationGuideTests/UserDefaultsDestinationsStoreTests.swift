0d321abe0d45fe4e6cfa44315652eabcfe0e3992
alexandre <alexandre.alcuvilla@bjtmail.com>
2022-09-15 22:05:22 +0200

    Setup UserDefaults destination store tests

---
+++ DestinationGuideTests/UserDefaultsDestinationsStoreTests.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultsStoreTests.swift
+//  DestinationGuideTests
+//
+//  Created by Alexandre BJT on 15/09/2022.
+//
+
+import XCTest
+
+class UserDefaultsDestinationsStoreTests: XCTestCase {
+
+    
+
+}
