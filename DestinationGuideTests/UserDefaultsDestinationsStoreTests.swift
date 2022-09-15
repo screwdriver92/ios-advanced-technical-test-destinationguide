600a6fc27e309d2fdcfb9fe861b36d599f51d73c
alexandre <alexandre.alcuvilla@bjtmail.com>
2022-09-15 22:31:19 +0200

    On init the store is empty

--- DestinationGuideTests/UserDefaultsDestinationsStoreTests.swift
+++ DestinationGuideTests/UserDefaultsDestinationsStoreTests.swift
@@ -6,9 +6,27 @@
 //
 
 import XCTest
+@testable import DestinationGuide
+
+//[✅] On init the store is empty
+//[ ] Update add all destination to the store
+//[ ] Delete the store remove all persisted destination
+//[ ] Get destination in the right order
+
+class UserDefaultsDestinationStore {
+  func getDestinations() -> [Destination] {
+    return []
+  }
+}
 
 class UserDefaultsDestinationsStoreTests: XCTestCase {
 
+  func test_getDestinations_persistDestinations() {
+    let sut = UserDefaultsDestinationStore()
     
+    let destinations = sut.getDestinations()
+    
+    XCTAssertTrue(destinations.isEmpty)
+  }
 
 }
